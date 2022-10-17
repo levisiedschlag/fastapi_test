01505af5088d1c1204c0b1672230490d88b402bc
levisatto <levi.siedschlag@fontespromotora.com.br>
2022-10-17 14:09:58 -0300

    delete artigo

--- api/v1/endpoints/artigo.py
+++ api/v1/endpoints/artigo.py
@@ -49,7 +49,7 @@ async def get_artigo(artigo_id:int ,db:AsyncSession=Depends(get_session)):
 @router.put('/{artigo_id}', response_model=ArtigoSchema, status_code=status.HTTP_202_ACCEPTED)
 async def put_artigo(artigo_id:int, artigo:ArtigoSchema, db:AsyncSession=Depends(get_session), usuario_logado:UsuarioModel=Depends(get_current_user)):
     async with db as session:
-        query=select(ArtigoModel).filter(ArtigoModel.id==artigo_id)
+        query=select(ArtigoModel).filter(ArtigoModel.id==artigo_id, ArtigoModel.usuario_id==usuario_logado.id)
         result=await session.execute(query)
         artigo_up:ArtigoModel=result.scalars().unique().one_or_none()
         if artigo_up:
@@ -62,6 +62,21 @@ async def put_artigo(artigo_id:int, artigo:ArtigoSchema, db:AsyncSession=Depends
             if artigo_up.url_fonte:
                 artigo_up.url_fonte=artigo.url_fonte
 
+            await session.commit()
             return artigo_up
         else:
-            raise HTTPException(detail='Artigo nao encontrado', status_code=status.HTTP_404_NOT_FOUND)
+            raise HTTPException(detail='Artigo não encontrado', status_code=status.HTTP_404_NOT_FOUND)
+
+# DELETE ARTIGO
+@router.delete('/{artigo_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_artigo(artigo_id:int, db:AsyncSession=Depends(get_session), usuario_logado:UsuarioModel=Depends(get_current_user)):
+    async with db as session:
+        query=select(ArtigoModel).filter(ArtigoModel.id==artigo_id).filter(ArtigoModel.usuario_id==usuario_logado.id)
+        result=await session.execute(query)
+        artigo_del:ArtigoModel=result.scalars().unique().one_or_none()
+        if artigo_del:
+            await session.delete(artigo_del)
+            await session.commit()
+            return Response(status_code=status.HTTP_204_NO_CONTENT)
+        else:
+            raise HTTPException(detail="Artigo não encontrado", status_code=status.HTTP_404_NOT_FOUND)
